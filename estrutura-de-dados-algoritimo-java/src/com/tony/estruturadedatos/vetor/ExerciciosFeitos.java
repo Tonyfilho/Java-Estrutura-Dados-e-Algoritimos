76babde2bc82be1175b365fb35e6a76fbf1ae5a1
tonyfilho <oficinadosdrones@gmail.com>
2024-04-23 13:24:37 +0100

    Exerc02.java

--- estrutura-de-dados-algoritimo-java/src/com/tony/estruturadedatos/vetor/ExerciciosFeitos.java
+++ estrutura-de-dados-algoritimo-java/src/com/tony/estruturadedatos/vetor/ExerciciosFeitos.java
@@ -1,22 +1,47 @@
 package com.tony.estruturadedatos.vetor;
 
-
-
+@SuppressWarnings("unchecked")
 public class ExerciciosFeitos<T> {
 
     // public ArrayList<T> localArray = new ArrayList<T>();
     private T[] localArray;
     private int tamanho;
 
-
     public ExerciciosFeitos(int elemento) {
         this.localArray = (T[]) new Object[elemento];
         this.tamanho = 0;
     }
-    
 
-    
-    public boolean add(T elemento) {    
+    /** Classe Array do Java*********************Usaremos o ToString */
+    /** Retornando somente os Elementos que existem no Vetor sem os Nulos */
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            s.append(this.localArray[i]);
+            s.append(", ");
+        }
+        if (this.tamanho > 0) {
+            s.append(this.localArray[this.tamanho - 1]);
+        }
+        s.append("]");
+        return s.toString();
+    }
+
+    private void aumentaCapacidade() {
+        if (this.localArray.length == this.tamanho) {
+            T[] localArrayNovos = (T[]) new Object[this.localArray.length * 2];
+            for (int i = 0; i < localArray.length; i++) {
+                localArrayNovos[i] = this.localArray[i];
+            }
+            this.localArray = localArrayNovos;
+        }
+    }
+
+    public boolean add(T elemento) {
+        this.aumentaCapacidade();
         if (this.tamanho < this.localArray.length) {
             this.localArray[tamanho] = elemento;
             tamanho++;
@@ -25,7 +50,7 @@ public class ExerciciosFeitos<T> {
             return false;
         }
     }
-    
+
     /**
      * 01 Exercicio é criar um metodo que mostra um conteudo no Array, sem usar a
      * classe Array list
@@ -46,15 +71,48 @@ public class ExerciciosFeitos<T> {
     /*********** Confirma e retorna um Int q é o Index do Elemento***** */
     public int containsInt(T elemento) {
         for (int index = 0; index < localArray.length; index++) {
-        if (this.localArray[index].equals(elemento)) {
-            System.out.println("Yes We have");
-            return index;            
-          }
+            if (this.localArray[index].equals(elemento)) {
+                System.out.println("Yes We have");
+                return index;
+            }
         }
         System.out.println("Nope We dont have");
         return -1;
     }
 
+    /**
+     * 02 Exercicio é criar um metodo q retorna o index do elemento, tipo
+     * LastIndexOf do ArrayList, sem usar a
+     * classe Array list
+     * 
+     */
 
+    // 1,2,3,1
+    public int lastIndexOfLocal(T elemento) {
+        int lastIndex = -1;
+        for (int i = 0; i < tamanho; i++) {
+            if (localArray[i].equals(elemento)) {
+                lastIndex = i;
+                System.out.println("uma x: " + lastIndex);
+
+            }
+
+        }
+        return lastIndex;
+    }
+    /**Esta é forma correta iterar do fim para inicio para evitar problema com desempenho
+     * Obs: Temos q por tamanho -1, para n dar erro 
+     */
+    public int lastIndexOfLocalInverso(T elemento) {
+        int lastIndex = -1;
+        for (int i = tamanho -1  ; i >= 0; i--) {
+            if (localArray[i].equals(elemento)) {
+              return  i;
+
+            }
+
+        }
+        return lastIndex;
+    }
 
 }
---
+++ estrutura-de-dados-algoritimo-java/src/com/tony/estruturadedatos/vetor/labs/Exerc02.java
@@ -0,0 +1,22 @@
+package com.tony.estruturadedatos.vetor.labs;
+
+import com.tony.estruturadedatos.vetor.ExerciciosFeitos;
+
+
+public class Exerc02 {
+
+    public static void main(String[] args) {
+        ExerciciosFeitos<Integer> exerciciosFeitos = new ExerciciosFeitos<Integer>(5);
+
+        exerciciosFeitos.add(1);
+        exerciciosFeitos.add(2);
+        exerciciosFeitos.add(3);
+        exerciciosFeitos.add(1);
+
+
+        System.out.println("Array: "+ exerciciosFeitos + " O Ultimo Index é: " + exerciciosFeitos.lastIndexOfLocal(1));
+        System.out.println("Array inverso: "+ exerciciosFeitos + " O Ultimo Index é : " + exerciciosFeitos.lastIndexOfLocalInverso(1));
+
+
+    }
+}
