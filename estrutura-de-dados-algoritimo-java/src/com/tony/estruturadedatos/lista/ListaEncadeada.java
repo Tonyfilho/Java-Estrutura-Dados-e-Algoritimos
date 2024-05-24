c4fe301c3dfa45e53a3ed351fdd766c235623690
tonyfilho <oficinadosdrones@gmail.com>
2024-05-24 13:34:13 +0100

    ListaEncadeada

--- estrutura-de-dados-algoritimo-java/src/com/tony/estruturadedatos/lista/ListaEncadeada.java
+++ estrutura-de-dados-algoritimo-java/src/com/tony/estruturadedatos/lista/ListaEncadeada.java
@@ -1,35 +1,44 @@
 package com.tony.estruturadedatos.lista;
 
-
 /**Lembrando que cada Lista Encadiada tem 1 NÓ e cada NÓ tem 2 informações. */
 /**
- * Colocamos a nos classe como Generica para ter qualquer tipo, Numero, string e Objetos.
+ * Colocamos a nos classe como Generica para ter qualquer tipo, Numero, string e
+ * Objetos.
  * 1ª Criar no metodo adciona, mas o Nó é algo Interno da Classe.
  */
-public class ListaEncadeada <T>{
-    private No<T> inicio;
+public class ListaEncadeada<T> {
+    private No<T> inicioNO;
+
+    private No<T> ultimoNO;
+
+    private int tamanho;
 
     public void adiciona(T elemento) {
-        No<T> celula = new No<T>(elemento);
-        this.inicio = celula;
+        No<T> celula = new No<T>(elemento); /**Criando o NO */
+        /** Tamanho esta VAZIO ? Atualiza o InicioNO, senão o UltimoNO. */
+        if (tamanho == 0) {
+            this.inicioNO = celula; /**
+                                     * Si Tamanho for ZERO ( 0 ) pegaremos o IncioNO e --> Apontaremos para Celula.
+                                     */
+        } else {
+            this.ultimoNO.setProximo(celula); /**
+                                                  * Se a Lista não tiver Vazia, ou seja tem Data, Setaremos o UltimoNO, Observe que a Variavel MUDOU de
+                                                  * COMPORTAMENTO
+                                                  */
+
+        }
+        this.ultimoNO = celula; /** Pegaremos o Ponteiro do UltimoNO --> e Apontaremos para Celula */
+        this.tamanho++;
 
     }
 
+    public int getTamanho() {
+        return this.tamanho;
+    }
 
-
-
-
-
-    
     @Override
     public String toString() {
-        return "ListaEncadiada.inicio=" + inicio + "]";
+        return "ListaEncadiada.inicio = " + inicioNO + "; ";
     }
-
-
-
-
-    
-  
 
 }
--- estrutura-de-dados-algoritimo-java/src/com/tony/estruturadedatos/lista/teste/ListaEncadeadaExec.java
+++ estrutura-de-dados-algoritimo-java/src/com/tony/estruturadedatos/lista/teste/ListaEncadeadaExec.java
@@ -9,8 +9,16 @@ public class ListaEncadeadaExec {
         ListaEncadeada <Integer> lista = new ListaEncadeada<>();
 
         lista.adiciona(1);
-
-        System.out.println("Nossa Lista " + lista);
         
+        System.out.println("\nNossa Lista \n" + lista + "\nTamanho da Lista " +  lista.getTamanho());
+        
+        lista.adiciona(2);
+        System.out.println("\nNossa Lista \n" + lista + "\nTamanho da Lista " +  lista.getTamanho());
+
+        lista.adiciona(3);
+        System.out.println("\nNossa Lista \n" + lista + "\nTamanho da Lista " +  lista.getTamanho());
+
+        lista.adiciona(4);
+        System.out.println("\nNossa Lista \n" + lista + "\nTamanho da Lista " +  lista.getTamanho());
     }
 }
